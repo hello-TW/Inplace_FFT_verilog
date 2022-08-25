f15490e653503066723c2fd0f026d3573cce97df
TW_Bite <xodnjsdl98@gmail.com>
2022-08-25 14:08:15 +0900

    gen_edit_220825

--- inplace_fft_dif_dpsram_r2_1stage_general/controlblock.v
+++ inplace_fft_dif_dpsram_r2_1stage_general/controlblock.v
@@ -7,18 +7,18 @@ module controlblock (
 );
 
     //temporary variables
-    wire bank_select_temp, bank_select_stage6;
-	wire [7:0] cnt_stage6, cnt_temp;
+    wire bank_select_temp, bank_select_stage6, bank_select_delay1;
+	wire [7:0] cnt_stage6, cnt_temp, cnt_temp_delay1;
     reg input_done_temp, output_start_temp, swap0_en_temp, swap1_en_temp;
     reg we_b0_temp, re_b0_temp, we_b1_temp, re_b1_temp;
     reg re_b0_reg, re_b1_reg;
-	reg [5:0] waddr_b0_temp, waddr_b1_temp, raddr_b0_temp, raddr_b1_temp;
+	reg [4:0] waddr_b0_temp, waddr_b1_temp, raddr_b0_temp, raddr_b1_temp, ref_addr;
     
     //register
     reg output_start_reg, swap0_en_reg, swap1_en_reg;
     reg [2:0] state;
-    reg [5:0] raddr_b0_reg, raddr_b1_reg;
-    reg signed [5:0] increment;
+    reg [4:0] raddr_b0_reg, raddr_b1_reg;
+    reg signed [4:0] increment;
 
     //FSM states
     parameter inout_state  = 3'd0;
@@ -60,7 +60,7 @@ always @(posedge clk) begin
 end
 
     //output
-    always @(state, cnt_temp, raddr_b0_reg, raddr_b1_reg) begin
+    always @(state, cnt_temp, cnt_temp_delay1, half_cnt, bank_select_temp, bank_select_stage6, bank_select_delay1) begin
         case(state)
             inout_state : begin
                 input_done_temp = 0;
@@ -74,8 +74,8 @@ end
                 re_b1_temp = 1;
                 waddr_b0_temp = cnt_temp>>1;
                 waddr_b1_temp = cnt_temp>>1;
-                raddr_b0_temp = cnt_temp[5:0]+1;
-                raddr_b1_temp = cnt_temp[5:0]+1;
+                raddr_b0_temp = cnt_temp[4:0]+1;
+                raddr_b1_temp = cnt_temp[4:0]+1;
             end
             input_state : begin
                 input_done_temp = 0;
@@ -95,92 +95,92 @@ end
             stage1_state : begin
                 input_done_temp = 1;
                 output_start_temp = 0;
-                swap0_en_temp = cnt_temp[4];
-                swap1_en_temp = cnt_temp[4];
-                increment = cnt_temp[4] ? -16 : 16;
+                swap0_en_temp = bank_select_delay1;
+                swap1_en_temp = bank_select_delay1;
                 we_b0_temp = re_b0_reg;
                 we_b1_temp = re_b0_reg;
                 re_b0_temp = 1;
                 re_b1_temp = 1;
+                ref_addr   = {0,cnt_temp[4:1]};
                 waddr_b0_temp = raddr_b0_reg;
                 waddr_b1_temp = raddr_b1_reg;
-                raddr_b0_temp = cnt_temp[5:0];
-                raddr_b1_temp = $unsigned($signed(cnt_temp[5:0]) + increment);
+                raddr_b0_temp = ref_addr + (bank_select_temp? 16 : 0);
+                raddr_b1_temp = ref_addr + (bank_select_temp? 0  : 16);
             end
             stage2_state : begin
                 input_done_temp = 1;
                 output_start_temp = 0;
-                swap0_en_temp = cnt_temp[3];
-                swap1_en_temp = cnt_temp[3];
-                increment = cnt_temp[3] ? -8 : 8;
+                swap0_en_temp = bank_select_delay1;
+                swap1_en_temp = bank_select_delay1;
                 we_b0_temp = 1;
                 we_b1_temp = 1;
                 re_b0_temp = 1;
                 re_b1_temp = 1;
+                ref_addr   = {cnt_temp[4],0,cnt_temp[3:1]};
                 waddr_b0_temp = raddr_b0_reg;
                 waddr_b1_temp = raddr_b1_reg;
-                raddr_b0_temp = cnt_temp[5:0];
-                raddr_b1_temp = $unsigned($signed(cnt_temp[5:0]) + increment);
+                raddr_b0_temp = ref_addr + (bank_select_temp? 8 : 0);
+                raddr_b1_temp = ref_addr + (bank_select_temp? 0 : 8);
             end
-stage3_state : begin
-    input_done_temp = 1;
-    output_start_temp = 0;
-    swap0_en_temp = cnt_temp[2];
-    swap1_en_temp = cnt_temp[2];
-    increment = cnt_temp[2] ? -4 : 4;
-    we_b0_temp = 1;
-    we_b1_temp = 1;
-    re_b0_temp = 1;
-    re_b1_temp = 1;
-    waddr_b0_temp = raddr_b0_reg;
-    waddr_b1_temp = raddr_b1_reg;
-    raddr_b0_temp = cnt_temp[5:0];
-    raddr_b1_temp = $unsigned($signed(cnt_temp[5:0]) + increment);
-end
-stage4_state : begin
-    input_done_temp = 1;
-    output_start_temp = 0;
-    swap0_en_temp = cnt_temp[1];
-    swap1_en_temp = cnt_temp[1];
-    increment = cnt_temp[1] ? -2 : 2;
-    we_b0_temp = 1;
-    we_b1_temp = 1;
-    re_b0_temp = 1;
-    re_b1_temp = 1;
-    waddr_b0_temp = raddr_b0_reg;
-    waddr_b1_temp = raddr_b1_reg;
-    raddr_b0_temp = cnt_temp[5:0];
-    raddr_b1_temp = $unsigned($signed(cnt_temp[5:0]) + increment);
-end
-stage5_state : begin
-    input_done_temp = 1;
-    output_start_temp = 0;
-    swap0_en_temp = cnt_temp[0];
-    swap1_en_temp = cnt_temp[0];
-    increment = cnt_temp[0] ? -1 : 1;
-    we_b0_temp = 1;
-    we_b1_temp = 1;
-    re_b0_temp = 1;
-    re_b1_temp = 1;
-    waddr_b0_temp = raddr_b0_reg;
-    waddr_b1_temp = raddr_b1_reg;
-    raddr_b0_temp = cnt_temp[5:0];
-    raddr_b1_temp = $unsigned($signed(cnt_temp[5:0]) + increment);
-end
+            stage3_state : begin
+                input_done_temp = 1;
+                output_start_temp = 0;
+                swap0_en_temp = bank_select_delay1;
+                swap1_en_temp = bank_select_delay1;
+                we_b0_temp = 1;
+                we_b1_temp = 1;
+                re_b0_temp = 1;
+                re_b1_temp = 1;
+                ref_addr   = {cnt_temp[4:3],0,cnt_temp[2:1]};
+                waddr_b0_temp = raddr_b0_reg;
+                waddr_b1_temp = raddr_b1_reg;
+                raddr_b0_temp = ref_addr + (bank_select_temp? 4 : 0);
+                raddr_b1_temp = ref_addr + (bank_select_temp? 0 : 4);
+            end
+            stage4_state : begin
+                input_done_temp = 1;
+                output_start_temp = 0;
+                swap0_en_temp = bank_select_delay1;
+                swap1_en_temp = bank_select_delay1;
+                we_b0_temp = 1;
+                we_b1_temp = 1;
+                re_b0_temp = 1;
+                re_b1_temp = 1;
+                ref_addr   = {cnt_temp[4:2], 0, cnt_temp[1]};
+                waddr_b0_temp = raddr_b0_reg;
+                waddr_b1_temp = raddr_b1_reg;
+                raddr_b0_temp = ref_addr + (bank_select_temp? 2 : 0);
+                raddr_b1_temp = ref_addr + (bank_select_temp? 0 : 2);
+            end
+            stage5_state : begin
+                input_done_temp = 1;
+                output_start_temp = 0;
+                swap0_en_temp = bank_select_delay1;
+                swap1_en_temp = bank_select_delay1;
+                we_b0_temp = 1;
+                we_b1_temp = 1;
+                re_b0_temp = 1;
+                re_b1_temp = 1;
+                ref_addr   = {cnt_temp[4:1],0};
+                waddr_b0_temp = raddr_b0_reg;
+                waddr_b1_temp = raddr_b1_reg;
+                raddr_b0_temp = half_cnt + (bank_select_temp? 1 : 0);
+                raddr_b1_temp = half_cnt + (bank_select_temp? 0 : 1);
+            end
             default : begin //stage6_stage
                 input_done_temp = 1;
-                output_start_temp = 1;
-                swap0_en_temp = 0;
-                swap1_en_temp = 0;
-                increment = 0;
+                output_start_temp = 0;
+                swap0_en_temp = bank_select_delay1;
+                swap1_en_temp = bank_select_delay1;
                 we_b0_temp = 1;
                 we_b1_temp = 1;
                 re_b0_temp = 1;
                 re_b1_temp = 1;
+                ref_addr   = cnt_temp[4:0];
                 waddr_b0_temp = raddr_b0_reg;
                 waddr_b1_temp = raddr_b1_reg;
-                raddr_b0_temp = cnt_temp[5:0];
-                raddr_b1_temp = $unsigned($signed(cnt_temp[5:0]) + increment);
+                raddr_b0_temp = ref_addr;
+                raddr_b1_temp = ref_addr;
             end
         endcase
     end
@@ -192,22 +192,22 @@ end
             re_b1_reg <= re_b1_temp;
             raddr_b0_reg <= raddr_b0_temp;
             raddr_b1_reg <= raddr_b1_temp;
-            swap0_en_reg <= swap0_en_temp;
-            swap1_en_reg <= swap1_en_temp;
             output_start_reg <= output_start_temp;
         end
     end
-
+    assign cnt_temp_delay1 = cnt_temp - 1;
     assign cnt_stage6 = cnt_temp + 1;
-    assign bank_select_stage6 = (cnt_stage6[5]^cnt_stage6[4])^((cnt_stage6[3]^cnt_stage6[2])^(cnt_stage6[1]^cnt_stage6[0]));
+
     assign bank_select_temp = (cnt_temp[5]^cnt_temp[4])^((cnt_temp[3]^cnt_temp[2])^(cnt_temp[1]^cnt_temp[0]));
+    assign bank_select_delay1 = (cnt_temp_delay1[5]^cnt_temp_delay1[4])^((cnt_temp_delay1[3]^cnt_temp_delay1[2])^(cnt_temp_delay1[1]^cnt_temp_delay1[0]));
+    assign bank_select_stage6 = (cnt_stage6[5]^cnt_stage6[4])^((cnt_stage6[3]^cnt_stage6[2])^(cnt_stage6[1]^cnt_stage6[0]));
 
 //assign port
 assign input_done = input_done_temp;
 assign output_start = output_start_reg;
 assign bank_select = bank_select_temp;
-assign swap0_en = swap0_en_reg;
-assign swap1_en = swap1_en_reg;
+assign swap0_en = swap0_en_temp;
+assign swap1_en = swap1_en_temp;
 assign we_b0 = we_b0_temp;
 assign re_b0 = re_b0_temp; 
 assign we_b1 = we_b1_temp; 
